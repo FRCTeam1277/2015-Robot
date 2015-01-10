d1f2eaeb0def08ae11b02b4fcf1003c194e3b0c6
Ben Navetta <ben.navetta@gmail.com>
2015-01-10 17:02:39 -0500

    Use Talons for drive train

--- src/org/team1277/robot/subsystems/DriveTrain.java
+++ src/org/team1277/robot/subsystems/DriveTrain.java
@@ -3,9 +3,9 @@ package org.team1277.robot.subsystems;
 import org.team1277.robot.RobotMap;
 import org.team1277.robot.commands.TankDrive;
 
-import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public final class DriveTrain extends Subsystem
@@ -17,8 +17,8 @@ public final class DriveTrain extends Subsystem
     
     public DriveTrain()
     {
-    	leftMotor = new Jaguar(RobotMap.LEFT_DRIVE_MOTOR);
-    	rightMotor = new Jaguar(RobotMap.RIGHT_DRIVE_MOTOR);
+    	leftMotor = new Talon(RobotMap.LEFT_DRIVE_MOTOR);
+    	rightMotor = new Talon(RobotMap.RIGHT_DRIVE_MOTOR);
     	drive = new RobotDrive(leftMotor, rightMotor);
     }
     
