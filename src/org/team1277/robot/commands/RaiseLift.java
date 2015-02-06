5e36f981d912c58acfaa4ba8e7d8184ca46a93df
cfandrews <cfandrews@outlook.com>
2015-02-05 21:26:38 -0500

    Added lift capabilities

---
+++ src/org/team1277/robot/commands/RaiseLift.java
@@ -0,0 +1,51 @@
+package org.team1277.robot.commands;
+
+import org.team1277.robot.Robot;
+import org.team1277.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class RaiseLift extends Command {
+
+	public int count = 0;
+    public RaiseLift() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.lift);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	count++;
+    	SmartDashboard.putInt("Hello", count);
+    	if (RobotMap.limitSwitchTop.get()) {
+    		Robot.lift.moveUp();
+    	}
+    	else {
+    		Robot.lift.moveStop();
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ src/org/team1277/robot/subsystems/LiftSubsystem.java
@@ -0,0 +1,40 @@
+package org.team1277.robot.subsystems;
+
+import org.team1277.robot.RobotMap;
+import org.team1277.robot.commands.DefaultLiftCommand;
+import org.team1277.robot.commands.TankDriveCommand;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class LiftSubsystem extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	public Relay winch = RobotMap.lift;
+
+	public DigitalInput limitSwitchTop = RobotMap.limitSwitchTop;
+	public DigitalInput limitSwitchBottom = RobotMap.limitSwitchBottom;
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new DefaultLiftCommand());
+    }
+    
+    public void moveUp() {
+    	winch.set(Relay.Value.kForward);
+    }
+    public void moveDown() {
+    	winch.set(Relay.Value.kReverse);
+    }
+    public void moveStop() {
+    	winch.set(Relay.Value.kOff);
+    }
+}
+
